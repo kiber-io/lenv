e61314c3327400ae8e76e4bf09a3a11c2feebdd6
kiber-io <60169133+kiber-io@users.noreply.github.com>
2024-11-11 23:46:38 +0400

    Prevent running intallers as admin; Setting JAVA_HOME and JAVAENV_HOME

--- linux_install.sh
+++ linux_install.sh
@@ -1,37 +1,55 @@
 #!/bin/bash
 
-repo="kiber-io/javaenv"
-rootDir="$HOME/.javaenv"
-binDir="$rootDir/bin"
-envVarPath="$binDir/javaenv"
-currentJdkPath="$rootDir/currentjdk/bin"
+set -e
 
-if [[ $(uname -m) == "x86_64" ]]; then
+ENV_JAVAENV_HOME="JAVAENV_HOME"
+ENV_JAVA_HOME="JAVA_HOME"
+ENV_PATH="PATH"
+
+javaenvHomePath="$HOME/.javaenv"
+javaenvHomeBinPath="$javaenvHomePath/bin"
+currentJdkPath="$javaenvHomePath/currentjdk"
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Error: Please run this script as a non-administrator."
+    exit 1
+fi
+
+if [ "$(uname -m)" = "x86_64" ]; then
     assetName="javaenv_linux64"
 else
     assetName="javaenv_linux32"
 fi
 
-latestRelease=$(curl -s "https://api.github.com/repos/$repo/releases/latest")
+latestRelease=$(curl -s "https://api.github.com/repos/kiber-io/javaenv/releases/latest")
 latestAsset=$(echo "$latestRelease" | grep -oP "(?<=\"browser_download_url\": \").*${assetName}\"" | tr -d '\"')
 
-if [[ -z $latestAsset ]]; then
+if [ -z "$latestAsset" ]; then
     echo "Error: Could not find the asset for $assetName in the latest release."
     exit 1
 fi
 
-mkdir -p "$binDir"
+mkdir -p "$javaenvHomeBinPath"
+mkdir -p "$currentJdkPath"
 
-downloadPath="$envVarPath"
+downloadPath="$javaenvHomeBinPath/javaenv"
 curl -L "$latestAsset" -o "$downloadPath"
-chmod +x "$downloadPath"
 
-if ! echo "$PATH" | grep -q "$binDir"; then
-    echo "export PATH=\"$binDir:\$PATH\"" >> ~/.bashrc
+path=$(echo "$PATH")
+if [[ ! "$path" == *"$ENV_JAVA_HOME/bin"* ]]; then
+    path="$javaenvHomePath/currentjdk/bin:$path"
+fi
+if [[ ! "$path" == *"$ENV_JAVAENV_HOME/bin"* ]]; then
+    path="$javaenvHomeBinPath:$path"
 fi
 
-if ! echo "$PATH" | grep -q "$currentJdkPath"; then
-    echo "export PATH=\"$currentJdkPath:\$PATH\"" >> ~/.bashrc
-fi
+export JAVAENV_HOME="$javaenvHomePath"
+export JAVA_HOME="$currentJdkPath"
+export PATH="$path"
 
-echo "Installation completed. Please restart your terminal or run 'source ~/.bashrc' to use 'javaenv' from the command line."
+echo "export JAVAENV_HOME=\"$javaenvHomePath\"" >> ~/.bashrc
+echo "export JAVA_HOME=\"$currentJdkPath\"" >> ~/.bashrc
+echo "export PATH=\"$javaenvHomeBinPath:\$JAVA_HOME/bin:\$PATH\"" >> ~/.bashrc
+echo "export JAVA_HOME=\"$currentJdkPath\"" >> ~/.profile
+
+echo "Installation completed. Please restart your terminal to start using javaenv."
--- win_install.ps1
+++ win_install.ps1
@@ -1,8 +1,17 @@
-$repo = "kiber-io/javaenv"
-$rootDir = "$env:USERPROFILE\.javaenv"
-$binDir = "$rootDir\bin"
-$envVarPath = "$binDir\javaenv.exe"
-$currentJdkPath = "$rootDir\currentjdk\bin"
+$ENV_JAVAENV_HOME = "JAVAENV_HOME"
+$ENV_JAVA_HOME = "JAVA_HOME"
+$ENV_PATH = "Path"
+
+$javaenvHomePath = "$env:USERPROFILE\.javaenv"
+$javaenvHomeBinPath = "$javaenvHomePath\bin"
+$currentJdkPath = "$javaenvHomePath\currentjdk"
+
+$isAdmin = ([Security.Principal.WindowsIdentity]::GetCurrent()).Groups -match 'S-1-5-32-544'
+
+if ($isAdmin) {
+    Write-Output "Error: Please run this script as a non-administrator."
+    exit
+}
 
 $assetName = if ([System.Environment]::Is64BitOperatingSystem) {
     "javaenv_win64.exe"
@@ -10,7 +19,7 @@ $assetName = if ([System.Environment]::Is64BitOperatingSystem) {
     "javaenv_win32.exe"
 }
 
-$latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/$repo/releases/latest"
+$latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/kiber-io/javaenv/releases/latest"
 $latestAsset = $latestRelease.assets | Where-Object { $_.name -eq $assetName }
 
 if (-not $latestAsset) {
@@ -18,20 +27,31 @@ if (-not $latestAsset) {
     exit 1
 }
 
-if (!(Test-Path -Path $binDir)) {
-    New-Item -ItemType Directory -Path $binDir | Out-Null
+# create directories only when asset is found to avoid creating unnecessary directories
+if (!(Test-Path -Path $javaenvHomeBinPath)) {
+    New-Item -ItemType Directory -Path $javaenvHomeBinPath | Out-Null
+}
+if (!(Test-Path -Path $currentJdkPath)) {
+    New-Item -ItemType Directory -Path $currentJdkPath | Out-Null
 }
 
-$downloadPath = "$binDir\javaenv.exe"
+$downloadPath = "$javaenvHomeBinPath\javaenv.exe"
 Invoke-WebRequest -Uri $latestAsset.browser_download_url -OutFile $downloadPath
 
-$currentPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
-if (-not $currentPath.Contains($currentJdkPath)) {
-    $currentPath = "$currentJdkPath;$currentPath"
+$path = [System.Environment]::GetEnvironmentVariable($ENV_PATH, [System.EnvironmentVariableTarget]::User)
+$javaHomePath = [System.Environment]::GetEnvironmentVariable($ENV_JAVA_HOME, [System.EnvironmentVariableTarget]::User)
+# if JAVA_HOME\bin is not in the path, add it
+if ($null -eq $javaHomePath -or -not $path.Contains("%$ENV_JAVA_HOME%\bin")) {
+    $path = "%$ENV_JAVA_HOME%\bin;$path"
 }
-if (-not $currentPath.Contains($binDir)) {
-    $currentPath = "$binDir;$currentPath"
-}
-[System.Environment]::SetEnvironmentVariable("Path", $currentPath, [System.EnvironmentVariableTarget]::User)
 
-Write-Output "Installation completed. You can now use 'javaenv' from the command line. Please restart your command line interface."
+$javaenvPath = [System.Environment]::GetEnvironmentVariable($ENV_JAVAENV_HOME, [System.EnvironmentVariableTarget]::User)
+# if JAVAENV_HOME\bin is not set or it is not in the path, add it
+if ($null -eq $javaenvPath -or -not $path.Contains("%$ENV_JAVAENV_HOME%\bin")) {
+    $path = "%$ENV_JAVAENV_HOME%\bin;$path"
+}
+[System.Environment]::SetEnvironmentVariable($ENV_JAVAENV_HOME, $javaenvHomePath, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable($ENV_JAVA_HOME, "%$ENV_JAVAENV_HOME%\currentjdk", [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable($ENV_PATH, $path, [System.EnvironmentVariableTarget]::User)
+
+Write-Output "Installation completed. Please restart your terminal to start using javaenv."
