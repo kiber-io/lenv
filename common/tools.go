222dda83d1f5fc24c916172c768186304f8a8401
kiber-io <60169133+kiber-io@users.noreply.github.com>
2024-11-11 23:27:56 +0400

    Removing unnecessary comments

--- common/tools.go
+++ common/tools.go
@@ -34,17 +34,11 @@ func GetPlatformPrefix(osName string, arch string) string {
 }
 
 func ParseAssetName(assetName string) string {
-	// Разделяем имя по символу "-"
 	parts := strings.Split(assetName, "-")
-
-	// Проверяем, что у нас достаточно частей после разделения
 	if len(parts) < 2 {
 		return ""
 	}
-
-	// Удаляем суффикс ".zip" из второй части
 	nameWithoutSuffix := strings.TrimSuffix(parts[1], ".zip")
-
 	return nameWithoutSuffix
 }
 
@@ -58,26 +52,22 @@ func FindVersion(versions []Version, targetVersion string, targetVendor string) 
 }
 
 func DownloadFile(url string) (string, error) {
-	// Выполняем HTTP GET-запрос
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", fmt.Errorf("failed to download file: %v", err)
 	}
 	defer resp.Body.Close()
 
-	// Проверяем успешность ответа
 	if resp.StatusCode != http.StatusOK {
 		return "", fmt.Errorf("bad status: %s", resp.Status)
 	}
 
-	// Создаем временный файл в системной временной директории
 	tmpFile, err := os.CreateTemp("", "javaenv-jdk-*.zip")
 	if err != nil {
 		return "", fmt.Errorf("failed to create temp file: %v", err)
 	}
 	defer tmpFile.Close()
 
-	// Копируем содержимое ответа в временный файл
 	_, err = io.Copy(tmpFile, resp.Body)
 	if err != nil {
 		return "", fmt.Errorf("failed to save file: %v", err)
@@ -87,44 +77,31 @@ func DownloadFile(url string) (string, error) {
 }
 
 func Unzip(src, dest string) error {
-	// Открываем ZIP-файл
 	r, err := zip.OpenReader(src)
 	if err != nil {
 		return fmt.Errorf("failed to open zip file: %v", err)
 	}
 	defer r.Close()
 
-	// Перебираем файлы в архиве
 	for _, f := range r.File {
-		// Определяем путь для распаковки файла
 		fpath := filepath.Join(dest, f.Name)
-
-		// Создаем директории для вложенных файлов
 		if f.FileInfo().IsDir() {
 			os.MkdirAll(fpath, os.ModePerm)
 			continue
 		}
-
-		// Создаем необходимые директории для файла
 		if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
 			return fmt.Errorf("failed to create directories: %v", err)
 		}
-
-		// Открываем файл из архива
 		rc, err := f.Open()
 		if err != nil {
 			return fmt.Errorf("failed to open file in zip: %v", err)
 		}
 		defer rc.Close()
-
-		// Создаем файл на диске
 		outFile, err := os.Create(fpath)
 		if err != nil {
 			return fmt.Errorf("failed to create file on disk: %v", err)
 		}
 		defer outFile.Close()
-
-		// Копируем содержимое из ZIP-файла в новый файл
 		_, err = io.Copy(outFile, rc)
 		if err != nil {
 			return fmt.Errorf("failed to write file to disk: %v", err)
