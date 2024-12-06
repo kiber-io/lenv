43c32df4a53b78b280affdffa2295942d51c7ea9
kiber-io <60169133+kiber-io@users.noreply.github.com>
2024-12-06 23:42:56 +0400

    Refactor linux installer

--- linux_install.sh
+++ linux_install.sh
@@ -1,56 +1,88 @@
 #!/bin/bash
 
-set -e
+DEBUG=false
 
-ENV_JAVAENV_HOME="JAVAENV_HOME"
-ENV_JAVA_HOME="JAVA_HOME"
-ENV_PATH="PATH"
+while getopts "d" opt; do
+  case $opt in
+    d) DEBUG=true ;;
+    *) echo "Invalid option"; exit 1 ;;
+  esac
+done
 
-javaenvHomePath="$HOME/.javaenv"
-javaenvHomeBinPath="$javaenvHomePath/bin"
-currentJdkPath="$javaenvHomePath/currentjdk"
+initialize_environment_variables() {
+  lenv_home_path="$HOME/.lenv"
+}
 
-if [ "$(id -u)" -eq 0 ]; then
+test_admin() {
+  if [ "$(id -u)" -eq 0 ]; then
     echo "Error: Please run this script as a non-administrator."
     exit 1
-fi
+  fi
+}
 
-if [ "$(uname -m)" = "x86_64" ]; then
-    assetName="javaenv_linux64"
-else
-    assetName="javaenv_linux32"
-fi
+get_asset_name() {
+  if [ "$(uname -m)" == "x86_64" ]; then
+    echo "lenv_linux64"
+  else
+    echo "lenv_linux32"
+  fi
+}
 
-latestRelease=$(curl -s "https://api.github.com/repos/kiber-io/javaenv/releases/latest")
-latestAsset=$(echo "$latestRelease" | grep -oP "(?<=\"browser_download_url\": \").*${assetName}\"" | tr -d '\"')
+get_latest_asset() {
+  latest_release=$(curl -s https://api.github.com/repos/kiber-io/lenv/releases/latest)
+  asset_name=$(get_asset_name)
+  download_url=$(echo "$latest_release" | grep -oP "(?<=\"browser_download_url\": \").*${asset_name}.*(?=\")")
 
-if [ -z "$latestAsset" ]; then
-    echo "Error: Could not find the asset for $assetName in the latest release."
+  if [ -z "$download_url" ]; then
+    echo "Error: Could not find the asset for $asset_name in the latest release."
     exit 1
-fi
+  fi
+  echo "$download_url"
+}
 
-mkdir -p "$javaenvHomeBinPath"
-mkdir -p "$currentJdkPath"
+new_directories() {
+  mkdir -p "$lenv_home_path/bin"
+  mkdir -p "$lenv_home_path/java/current"
+}
 
-downloadPath="$javaenvHomeBinPath/javaenv"
-curl -L "$latestAsset" -o "$downloadPath"
-chmod +x "$downloadPath"
+get_asset() {
+  if [ "$DEBUG" = true ]; then
+    local_file_path="$PWD/build/$(get_asset_name)"
+    cp "$local_file_path" "$lenv_home_path/bin/lenv"
+  else
+    download_url=$(get_latest_asset)
+    curl -L "$download_url" -o "$lenv_home_path/bin/lenv"
+  fi
+  chmod +x "$lenv_home_path/bin/lenv"
+}
 
-path=$(echo "$PATH")
-if [[ ! "$path" == *"$ENV_JAVA_HOME/bin"* ]]; then
-    path="$javaenvHomePath/currentjdk/bin:$path"
-fi
-if [[ ! "$path" == *"$ENV_JAVAENV_HOME/bin"* ]]; then
-    path="$javaenvHomeBinPath:$path"
-fi
+update_environment_variables() {
+  profile_file="$HOME/.profile"
 
-export JAVAENV_HOME="$javaenvHomePath"
-export JAVA_HOME="$currentJdkPath"
-export PATH="$path"
+  ensure_newline() {
+    file=$1
+    [ -s "$file" ] && tail -c1 "$file" | read -r _ || echo >> "$file"
+  }
 
-echo "export JAVAENV_HOME=\"$javaenvHomePath\"" >> ~/.bashrc
-echo "export JAVA_HOME=\"$currentJdkPath\"" >> ~/.bashrc
-echo "export PATH=\"$javaenvHomeBinPath:\$JAVA_HOME/bin:\$PATH\"" >> ~/.bashrc
-echo "export JAVA_HOME=\"$currentJdkPath\"" >> ~/.profile
+  ensure_newline "$profile_file"
+  if ! grep -q "export LENV_HOME=$lenv_home_path" "$profile_file"; then
+    echo "export LENV_HOME=$lenv_home_path" >> "$profile_file"
+  fi
+  if ! grep -q "export PATH=\$LENV_HOME/bin:\$JAVA_HOME/bin:\$PATH" "$profile_file"; then
+    echo "export PATH=\$LENV_HOME/bin:\$JAVA_HOME/bin:\$PATH" >> "$profile_file"
+  fi
+  if ! grep -q "export JAVA_HOME=\$LENV_HOME/java/current" "$profile_file"; then
+    echo "export JAVA_HOME=\$LENV_HOME/java/current" >> "$profile_file"
+  fi
+}
 
-echo "Installation completed. Please restart your terminal to start using javaenv."
+main() {
+  test_admin
+  initialize_environment_variables
+  new_directories
+  get_asset
+  update_environment_variables
+  echo "Installation completed. Please restart your terminal to start using lenv."
+}
+
+main
